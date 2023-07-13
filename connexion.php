fba24ea35fbf9ea97c4e5f38055142b1f60a0914
TonTonHigo <hugoh420@outlook.fr>
2023-07-13 13:12:12 +0400

    insertion update delete fontionnels

--- connexion.php
+++ connexion.php
@@ -18,7 +18,7 @@ class ma_connexion{
 
         try {
 
-            $dsn = "mysql:host=" . $this-> host . "; dbname=" . $this-> nom_bd . "; charset:=utf8mb4_0900_ai_ci";
+            $dsn = "mysql:host=" . $this-> host . "; dbname=" . $this-> nom_bd . "; charset:=utf8mb4";
             $this-> connexionPDO = new PDO($dsn, $this-> user, $this-> password);
             $this-> connexionPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
--- index.php
+++ index.php
@@ -38,104 +38,90 @@
 
 
         <?php
+
+        
             $connexion = new ma_connexion("localhost", "app_organisation_contacts", "root", "");
-            $afficher = $connexion -> select("*","contacts");
-            foreach($afficher as $ligne) {
+            $afficher = $connexion->select("*", "contacts");
+
+            foreach ($afficher as $ligne) {
                 echo '
                     <tr>
                         <td><strong>' . $ligne['nom'] . '</strong></td>
-                        <td>'. $ligne['prenom'] .'</td>
-                        <td>'. $ligne['email'] .'</td>
-                        <td>'. $ligne['age'] .'</td>
+                        <td>' . $ligne['prenom'] . '</td>
+                        <td>' . $ligne['email'] . '</td>
+                        <td>' . $ligne['age'] . '</td>
                         <td>
                             <!-- Button trigger modal -->
                             <button type="button" class="btn colorange" data-bs-toggle="modal" data-bs-target="#update' . $ligne['id_contacts'] . '">
-                            MAJ
+                                MAJ
                             </button>
                             <button type="button" class="btn colred" data-bs-toggle="modal" data-bs-target="#delete' . $ligne['id_contacts'] . '">
-                            SUPPR
-                        </button>
+                                SUPPR
+                            </button>
                         </td>
                     </tr>
 
                     <!-- Modal UPDATE-->
                     <div class="modal fade" id="update' . $ligne['id_contacts'] . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Mise à jour d`un contact</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                        
-                            <p>Ête-vous sûr de vouloir mettre à jour ' . $ligne['nom'] . ' ' . $ligne['prenom'] . ' ' . $ligne['email'] . ' agé de ' . $ligne['age'] . ' ans ?</p>
-
-                            <div class="login">
-                                <form id="formupdate" action="update.php" method="POST">
-                                    <div class="mb-3">
-
-                                        <input type="number" class="form-control" hidden value="' . $ligne['id_contacts'] . '" name="id">
-
-                                        <label for="nom" class="form-label">Nom</label>
-                                        <input type="text" class="form-control" id="nom" aria-describedby="emailHelp" name="nom">
-
-                                        <label for="prenom" class="form-label">Prénom</label>
-                                        <input type="text" class="form-control" id="prenom" aria-describedby="emailHelp" name="prenom">
-
-                                        <label for="email" class="form-label">Email address</label>
-                                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
-
-                                        <label for="age" class="form-label">Age</label>
-                                        <input type="number" class="form-control" id="age" aria-describedby="emailHelp" name="age">
-
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Mise à jour d`un contact</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Êtes-vous sûr de vouloir mettre à jour ' . $ligne['nom'] . ' ' . $ligne['prenom'] . ' ' . $ligne['email'] . ' âgé de ' . $ligne['age'] . ' ans ?</p>
+                                    <div class="login">
+                                        <form id="formupdate' . $ligne['id_contacts'] . '" action="update.php" method="POST">
+                                            <div class="mb-3">
+                                                <input type="number" class="form-control" hidden value="' . $ligne['id_contacts'] . '" name="id">
+                                                <label for="nom" class="form-label">Nom</label>
+                                                <input type="text" class="form-control" id="nom" aria-describedby="emailHelp" name="nom">
+                                                <label for="prenom" class="form-label">Prénom</label>
+                                                <input type="text" class="form-control" id="prenom" aria-describedby="emailHelp" name="prenom">
+                                                <label for="email" class="form-label">Email address</label>
+                                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+                                                <label for="age" class="form-label">Age</label>
+                                                <input type="number" class="form-control" id="age" aria-describedby="emailHelp" name="age">
+                                            </div>
+                                        </form>
                                     </div>
-                                </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                                    <button type="submit" class="btn colorange" form="formupdate' . $ligne['id_contacts'] . '">Mettre à jour</button>
+                                </div>
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                            <button type="submit" class="btn colorange" form="formupdate">Mettre à jour</button>
-                        </div>
-                        </div>
-                    </div>
                     </div>
 
                     <!-- Modal DELETE-->
                     <div class="modal fade" id="delete' . $ligne['id_contacts'] . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Supprimer un contact</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="login">
-                            
-                                <p>Ête-vous sûr de vouloir supprimer <br>' . $ligne['nom'] . ' <br> ' . $ligne['prenom'] . ' <br> ' . $ligne['email'] . '<br> agé de ' . $ligne['age'] . ' ans ?</p>
-
-                                <form id="formdelete" action="delete.php" method="POST">
-                                    <div class="mb-3">
-
-                                        <input type="number" class="form-control" value="' . $ligne['id_contacts'] . '" name="id" hidden>
-
-                
-                                                    
-                                    </div>s
-                                </form>
-
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Supprimer un contact</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="login">
+                                        <p>Êtes-vous sûr de vouloir supprimer <br>' . $ligne['nom'] . ' <br> ' . $ligne['prenom'] . ' <br> ' . $ligne['email'] . '<br> âgé de ' . $ligne['age'] . ' ans ?</p>
+                                        <form id="formdelete' . $ligne['id_contacts'] . '" action="delete.php" method="POST">
+                                            <div class="mb-3">
+                                                <input type="number" class="form-control" value="' . $ligne['id_contacts'] . '" name="id" hidden>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                                    <button type="submit" class="btn colred" form="formdelete' . $ligne['id_contacts'] . '">Supprimer</button>
+                                </div>
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                            <button type="submit" class="btn colred" form="formdelete">Supprimer</button>
-                        </div>
-                        </div>
-                    </div>
                     </div>
                 ';
-
             }
-
 
         ?>
             </tbody>
